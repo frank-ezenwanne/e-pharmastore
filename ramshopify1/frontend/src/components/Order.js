1a54f0ae8979c72febe9d6ae24b9fa61b5fd0341
Frank Ezenwanne <efrank272@gmail.com>
2023-05-02 20:36:19 +0100

    changes

--- ramshopify1/frontend/src/components/Order.js
+++ ramshopify1/frontend/src/components/Order.js
@@ -908,33 +908,35 @@ map_stuff = (num) =>{
                 name="total"
                 value = {this.state[num]["total"] ||''} 
             />
-            {this.props.all_loaded_serials[num] === true? <TailSpin
-                    height="20"
-                    width="20"
-                    color="#4fa94d"
-                    ariaLabel="tail-spin-loading"
-                    radius="1"
-                    wrapperStyle={{}}
-                    wrapperClass="tailspin-class"
-                    visible={true}
-                /> : this.props.all_loaded_serials[num] === false?
-                <div className="d-flex"><img className = 'svg-tick-error-class' src={Tick}/></div>
-                :this.props.all_loaded_serials[num] === "error"? 
-                (<div className = 'd-flex svg-tick-error-class' onClick = {()=>{
-                    this.props.send_orderproduct( item["product_id"],
-                    item["unit"],
-                    item["selected_unit"], item["cost"],
-                    item["raw_cost"],item["quantity_ordered"],
-                    item["full_pack_quantity"],item["unit_quantity"],
-                    item["total"],item['extra_info'],
-                    num,this.props.last_orderid)  }}>  {/* end of sendop func*/}
-                                                         
-                    {/* start div content */}
-                    <svg xmlns="http://www.w3.org/2000/svg"  fill="red" width="100%" height="100%" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.094l-4.157-4.104 4.1-4.141-1.849-1.849-4.105 4.159-4.156-4.102-1.833 1.834 4.161 4.12-4.104 4.157 1.834 1.832 4.118-4.159 4.143 4.102 1.848-1.849z"/></svg>
-                    {/* end */}
+         
+                {this.props.all_loaded_serials[num] === true? <TailSpin
+                        height="20"
+                        width="20"
+                        color="#4fa94d"
+                        ariaLabel="tail-spin-loading"
+                        radius="1"
+                        wrapperStyle={{}}
+                        wrapperClass="tailspin-class"
+                        visible={true}
+                    /> : this.props.all_loaded_serials[num] === false?
+                    <div className="d-flex"><img className = 'svg-tick-error-class' src={Tick}/></div>
+                    :this.props.all_loaded_serials[num] === "error"? 
+                    (<div className = 'd-flex svg-tick-error-class' onClick = {()=>{
+                        this.props.send_orderproduct( item["product_id"],
+                        item["unit"],
+                        item["selected_unit"], item["cost"],
+                        item["raw_cost"],item["quantity_ordered"],
+                        item["full_pack_quantity"],item["unit_quantity"],
+                        item["total"],item['extra_info'],
+                        num,this.props.last_orderid)  }}>  {/* end of sendop func*/}
+                                                            
+                        {/* start div content */}
+                        <svg xmlns="http://www.w3.org/2000/svg"  fill="red" width="100%" height="100%" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.094l-4.157-4.104 4.1-4.141-1.849-1.849-4.105 4.159-4.156-4.102-1.833 1.834 4.161 4.12-4.104 4.157 1.834 1.832 4.118-4.159 4.143 4.102 1.848-1.849z"/></svg>
+                        {/* end */}
 
-                </div>) :null
-             }    {/* end of loading serials/svg section */}
+                    </div>) :null
+                }
+              {/* end of loading serials/svg section */}
                
             
         </div> 
